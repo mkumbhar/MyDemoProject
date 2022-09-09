cc788594bab5e3130306f5d04441d3b918d4607a
mkumbhar <mkumbhar@vignetcorp.com>
2022-09-09 00:05:59 -0400

    fastlane and pod setup

--- MyDemoProject/MyDemoProject/ViewController.swift
+++ MyDemoProject/MyDemoProject/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var label : UILabel = {
+        let lbl = UILabel()
+        lbl.text = "My Demo App"
+        self.view.addSubview(lbl)
+        return lbl
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,6 +29,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func updateViewConstraints() {
+        label.snp.updateConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
+        super.updateViewConstraints()
+    }
 
 }
 
